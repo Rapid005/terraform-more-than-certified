166ded2a40eb77760033c2f95bfdbfdb4b42d4fa
Rapid005 <37708705+Rapid005@users.noreply.github.com>
2023-07-27 16:07:02 +0530

    Update main.tf bind_mount volume
    volume added to the nodered container

--- main.tf
+++ main.tf
@@ -9,11 +9,17 @@ terraform {
 
 provider "docker" {}
 
+resource "null_resource" "dockervol" {
+  provisioner "local-exec" {
+    command = "mkdir noderedvol/ || true && sudo chown -R 1000:1000 noderedvol/"
+  }
+}
+
 resource "random_string" "random" {
-  count = var.container_count
-  length = 4
+  count   = var.container_count
+  length  = 4
   special = false
-  upper = false
+  upper   = false
 }
 
 resource "docker_image" "nodered_image" {
@@ -22,10 +28,14 @@ resource "docker_image" "nodered_image" {
 
 resource "docker_container" "nodered" {
   count = var.container_count
-  name  = join("-",["nodered-container",random_string.random[count.index].result])
+  name  = join("-", ["nodered-container", random_string.random[count.index].result])
   image = docker_image.nodered_image.name
   ports {
     internal = var.int_port
     external = var.ext_port
+  }
+  volumes {
+    container_path = "/data"
+    host_path = "/root/terraform-docker/terraform-more-than-certified/noderedvol"
   }
 }
