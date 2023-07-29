1e323270d80939ea0252dd4f9a27c3ff2f087e13
Rapid005 <37708705+Rapid005@users.noreply.github.com>
2023-07-29 11:22:23 +0530

    Update outputs.tf
    Proper formating with container id and ip-address along with port values in json format.

--- modular-deployments/outputs.tf
+++ modular-deployments/outputs.tf
@@ -1,9 +1,4 @@
-#output "image_name" {
-#  value = module.image.image_out
-#}
-#output "container-name" {
-#value = flatten(module.container[*].container-name)
-#}
-#output "ip-address" {
-#value = flatten(module.container[*].ip-address)
-#}
+output "application_access" {
+  value       = module.container[*]
+  description = "The ports and ip-address for the container"
+}
