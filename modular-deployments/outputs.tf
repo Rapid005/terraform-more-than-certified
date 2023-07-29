7d09fe207b482f07d16431f93248aef8a0133478
Rapid005 <37708705+Rapid005@users.noreply.github.com>
2023-07-29 07:19:15 +0530

    Update outputs.tf

--- modular-deployments/outputs.tf
+++ modular-deployments/outputs.tf
@@ -1,9 +1,9 @@
 output "image_name" {
-  value = docker_image.nodered_image.name
+  value = module.image.image_out
 }
-output "container_name_1st" {
-  value = docker_container.nodered[*].name
+output "container-name" {
+  value = flatten(module.container[*].container-name)
 }
-output "ip_addresss" {
-  value = [for i in docker_container.nodered[*]: join( ":", i.network_data[*].ip_address, i.ports[*].external  )]
+output "ip-address" {
+  value = flatten(module.container[*].ip-address)
 }
