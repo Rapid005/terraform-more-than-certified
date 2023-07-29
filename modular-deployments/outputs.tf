33b044745e5e51823b8907f32b7ed6787a153b09
Rapid005 <37708705+Rapid005@users.noreply.github.com>
2023-07-29 10:13:55 +0530

    Update outputs.tf
    During for_each deployment.

--- modular-deployments/outputs.tf
+++ modular-deployments/outputs.tf
@@ -1,9 +1,9 @@
-output "image_name" {
-  value = module.image.image_out
-}
-output "container-name" {
-  value = flatten(module.container[*].container-name)
-}
-output "ip-address" {
-  value = flatten(module.container[*].ip-address)
-}
+#output "image_name" {
+#  value = module.image.image_out
+#}
+#output "container-name" {
+#value = flatten(module.container[*].container-name)
+#}
+#output "ip-address" {
+#value = flatten(module.container[*].ip-address)
+#}
