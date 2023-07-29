4685fdd9df85615c6461df5557c5d2cdb184834c
Rapid005 <37708705+Rapid005@users.noreply.github.com>
2023-07-29 11:19:39 +0530

    Update main.tf
    After adding grafana container specification.

--- modular-deployments/main.tf
+++ modular-deployments/main.tf
@@ -2,17 +2,24 @@ locals {
   deployment = {
     nodered = {
       container_count = length(var.ext_port["nodered"][terraform.workspace])
-      image          = var.image["nodered"][terraform.workspace]
-      int_port       = 1880
-      ext_port       = var.ext_port["nodered"][terraform.workspace]
-      container_path = "/data"
+      image           = var.image["nodered"][terraform.workspace]
+      int_port        = 1880
+      ext_port        = var.ext_port["nodered"][terraform.workspace]
+      container_path  = "/data"
     }
     influxdb = {
       container_count = length(var.ext_port["influxdb"][terraform.workspace])
-      image          = var.image["influxdb"][terraform.workspace]
-      int_port       = 8086
-      ext_port       = var.ext_port["influxdb"][terraform.workspace]
-      container_path = "/var/lib/influxdb"
+      image           = var.image["influxdb"][terraform.workspace]
+      int_port        = 8086
+      ext_port        = var.ext_port["influxdb"][terraform.workspace]
+      container_path  = "/var/lib/influxdb"
+    }
+    grafana = {
+      container_count = length(var.ext_port["grafana"][terraform.workspace])
+      image           = var.image["grafana"][terraform.workspace]
+      int_port        = 3000
+      ext_port        = var.ext_port["grafana"][terraform.workspace]
+      container_path  = "/var/lib/grafana"
     }
   }
 }
@@ -24,9 +31,9 @@ module "image" {
 }
 
 module "container" {
-  source   = "./container"
-  for_each = local.deployment
-  count_in             = each.value.container_count
+  source            = "./container"
+  for_each          = local.deployment
+  count_in          = each.value.container_count
   name_in           = each.key
   image_in          = module.image[each.key].image_out
   int_port_in       = each.value.int_port
