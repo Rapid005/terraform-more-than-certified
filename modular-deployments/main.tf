60421ae11b02cef94338c18a54a3887f9a18c6fe
Rapid005 <37708705+Rapid005@users.noreply.github.com>
2023-07-29 10:13:03 +0530

    Update main.tf
    After modularise multi container using for_each deployment model.

--- modular-deployments/main.tf
+++ modular-deployments/main.tf
@@ -1,29 +1,35 @@
-resource "null_resource" "dockervol" {
-  provisioner "local-exec" {
-    command = "mkdir noderedvol/ || true && sudo chown -R 1000:1000 noderedvol/"
+locals {
+  deployment = {
+    nodered = {
+      container_count = length(var.ext_port["nodered"][terraform.workspace])
+      image          = var.image["nodered"][terraform.workspace]
+      int_port       = 1880
+      ext_port       = var.ext_port["nodered"][terraform.workspace]
+      container_path = "/data"
+    }
+    influxdb = {
+      container_count = length(var.ext_port["influxdb"][terraform.workspace])
+      image          = var.image["influxdb"][terraform.workspace]
+      int_port       = 8086
+      ext_port       = var.ext_port["influxdb"][terraform.workspace]
+      container_path = "/var/lib/influxdb"
+    }
   }
-}
-
-resource "random_string" "random" {
-  count   = local.container_count
-  length  = 4
-  special = false
-  upper   = false
 }
 
 module "image" {
   source   = "./image"
-  image_in = var.image[terraform.workspace]
+  for_each = local.deployment
+  image_in = each.value.image
 }
 
 module "container" {
-  source            = "./container"
-  depends_on        = [null_resource.dockervol]
-  count             = local.container_count
-  name_in           = join("-", ["nodered", terraform.workspace, random_string.random[count.index].result])
-  image_in          = module.image.image_out
-  int_port_in       = var.int_port
-  ext_port_in       = var.ext_port[terraform.workspace][count.index]
-  container_path_in = "/data"
-  host_path_in      = "${path.cwd}/noderedvol"
+  source   = "./container"
+  for_each = local.deployment
+  count_in             = each.value.container_count
+  name_in           = each.key
+  image_in          = module.image[each.key].image_out
+  int_port_in       = each.value.int_port
+  ext_port_in       = each.value.ext_port
+  container_path_in = each.value.container_path
 }
