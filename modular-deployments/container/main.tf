e25b14f732ea5e645a5b536ce1b6b0a67a9f7450
Rapid005 <37708705+Rapid005@users.noreply.github.com>
2023-07-29 15:56:41 +0530

    Update main.tf
    After adding provisioner to the container.

--- modular-deployments/container/main.tf
+++ modular-deployments/container/main.tf
@@ -1,11 +1,10 @@
 resource "random_string" "random" {
   count   = var.count_in
-  #for_each = local.deployment
-  length   = 4
-  special  = false
-  upper    = false
+  length  = 4
+  special = false
+  upper   = false
 }
-resource "docker_container" "nodered" {
+resource "docker_container" "app" {
   count = var.count_in
   name  = join("-", [var.name_in, terraform.workspace, random_string.random[count.index].result])
   image = var.image_in
@@ -15,14 +14,34 @@ resource "docker_container" "nodered" {
   }
   volumes {
     container_path = var.container_path_in
-    volume_name    = docker_volume.container_volume[ count.index].name
+    volume_name    = docker_volume.container_volume[count.index].name
   }
+  provisioner "local-exec" {
+    #command = "echo ${{for x in self: x.name => join(":", x.network_data[*]["ip_address"],x.ports[*]["external"])}} > ${path.cwd}/../container.txt"
+    command = "echo ${self.name}: ${join("", [for x in self.network_data[*]["ip_address"] : x])}:${join("", self.ports[*]["external"])}  >  ${path.cwd}/../container.txt"
+  }
+  provisioner "local-exec" {
+    when       = destroy
+    command    = "rm -rvf ${path.cwd}/../container.txt"
+    on_failure = fail
+  }
+
 }
 
 resource "docker_volume" "container_volume" {
   count = var.count_in
-  name = "${var.name_in}-${random_string.random[count.index].result}-volume"
+  name  = "${var.name_in}-${random_string.random[count.index].result}-volume"
   lifecycle {
     prevent_destroy = false
+  }
+  provisioner "local-exec" {
+    when       = destroy
+    command    = "mkdir ${path.cwd}/../backup"
+    on_failure = continue
+  }
+  provisioner "local-exec" {
+    when       = destroy
+    command    = "sudo tar -czvf ${path.cwd}/../backup/${self.name}.tar.gz ${self.mountpoint}/"
+    on_failure = fail
   }
 }
