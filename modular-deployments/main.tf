30b3e3a888282514ed0cb7b710c24203e4d0c4b3
Rapid005 <37708705+Rapid005@users.noreply.github.com>
2023-07-30 05:57:16 +0530

    Update main.tf
    Update after adding multi volume setup in container.

--- modular-deployments/main.tf
+++ modular-deployments/main.tf
@@ -5,12 +5,12 @@ module "image" {
 }
 
 module "container" {
-  source            = "./container"
-  for_each          = local.deployment
-  count_in          = each.value.container_count
-  name_in           = each.key
-  image_in          = module.image[each.key].image_out
-  int_port_in       = each.value.int_port
-  ext_port_in       = each.value.ext_port
-  container_path_in = each.value.container_path
+  source      = "./container"
+  for_each    = local.deployment
+  count_in    = each.value.container_count
+  name_in     = each.key
+  image_in    = module.image[each.key].image_out
+  int_port_in = each.value.int_port
+  ext_port_in = each.value.ext_port
+  volumes_in  = each.value.volumes
 }
