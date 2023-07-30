5173ab026ffb86a76f83713e61111fad8a8264b6
Rapid005 <37708705+Rapid005@users.noreply.github.com>
2023-07-30 06:01:19 +0530

    Update main.tf
    Updated after volume module addition.

--- modular-deployments/container/main.tf
+++ modular-deployments/container/main.tf
@@ -12,13 +12,15 @@ resource "docker_container" "app" {
     internal = var.int_port_in
     external = var.ext_port_in[count.index]
   }
-  volumes {
-    container_path = var.container_path_in
-    volume_name    = docker_volume.container_volume[count.index].name
+  dynamic "volumes" {
+    for_each = var.volumes_in
+    content {
+      container_path = volumes.value["container_path_each"]
+      volume_name    = module.volume[count.index].volume_output[volumes.key]
+    }
   }
   provisioner "local-exec" {
-    #command = "echo ${{for x in self: x.name => join(":", x.network_data[*]["ip_address"],x.ports[*]["external"])}} > ${path.cwd}/../container.txt"
-    command = "echo ${self.name}: ${join("", [for x in self.network_data[*]["ip_address"] : x])}:${join("", self.ports[*]["external"])}  >  ${path.cwd}/../container.txt"
+    command = "echo ${self.name}: ${join("", self.network_data[*]["ip_address"])}:${join("", self.ports[*]["external"])}  >>  ${path.cwd}/../container.txt"
   }
   provisioner "local-exec" {
     when       = destroy
@@ -28,20 +30,9 @@ resource "docker_container" "app" {
 
 }
 
-resource "docker_volume" "container_volume" {
-  count = var.count_in
-  name  = "${var.name_in}-${random_string.random[count.index].result}-volume"
-  lifecycle {
-    prevent_destroy = false
-  }
-  provisioner "local-exec" {
-    when       = destroy
-    command    = "mkdir ${path.cwd}/../backup"
-    on_failure = continue
-  }
-  provisioner "local-exec" {
-    when       = destroy
-    command    = "sudo tar -czvf ${path.cwd}/../backup/${self.name}.tar.gz ${self.mountpoint}/"
-    on_failure = fail
-  }
+module "volume" {
+  source       = "./volume"
+  count        = var.count_in
+  volume_count = length(var.volumes_in)
+  volume_name  = join("-", [var.name_in, terraform.workspace, random_string.random[count.index].result, "volume"])
 }
