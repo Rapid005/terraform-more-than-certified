eba402e48c4986e41272748f56f8f8b55bafd34e
Rapid005 <37708705+Rapid005@users.noreply.github.com>
2023-07-30 05:58:04 +0530

    Update locals.tf
    After adding multi volume path for the container.

--- modular-deployments/locals.tf
+++ modular-deployments/locals.tf
@@ -5,21 +5,29 @@ locals {
       image           = var.image["nodered"][terraform.workspace]
       int_port        = 1880
       ext_port        = var.ext_port["nodered"][terraform.workspace]
-      container_path  = "/data"
+      volumes = [
+        { container_path_each = "/data" }
+      ]
     }
     influxdb = {
       container_count = length(var.ext_port["influxdb"][terraform.workspace])
       image           = var.image["influxdb"][terraform.workspace]
       int_port        = 8086
       ext_port        = var.ext_port["influxdb"][terraform.workspace]
-      container_path  = "/var/lib/influxdb"
+      volumes = [
+        { container_path_each = "/var/lib/influxdb" }
+      ]
     }
     grafana = {
       container_count = length(var.ext_port["grafana"][terraform.workspace])
       image           = var.image["grafana"][terraform.workspace]
       int_port        = 3000
       ext_port        = var.ext_port["grafana"][terraform.workspace]
-      container_path  = "/var/lib/grafana"
+      #container_path  = "/var/lib/grafana"
+      volumes = [
+        { container_path_each = "/var/lib/grafana" },
+        { container_path_each = "/etc/grafana" }
+      ]
     }
   }
 }
